e08596557f36646e93ee36ccbc77743b39d42792
Vita Midori <hi@vitamidori.com>
2023-11-08 16:34:13 +0100

    Guess target range units returned by Nightscout

--- src/app.ts
+++ src/app.ts
@@ -164,13 +164,17 @@ async function getSettings(nightscoutUrl: string, token: string): Promise<Settin
   console.log(`Fetching settings...`)
   const response = await fetch(`${nightscoutUrl}/api/v1/status.json?token=${token}`, { signal: AbortSignal.timeout(TIMEOUT_STATUS) })
   const status = await response.json()
-  const multiplier = status.settings.units === "mmol" ? MMOL_TO_MGDL : 1
+  // Convert mmol/l to mg/dl if necessary. It's not clear when Nightscout returns the target range in mmol/l and when in mg/dl.
+  // See the discussion in https://github.com/vitawasalreadytaken/glucoscape/pull/3
+  // We guess the units from the target range values: CGMs typically can't measure values over 30 mmol/l,
+  // and at the same time it makes no sense to set the target range top below 30 mg/dl (1.6 mmol/l).
+  const targetRangeConversionToMgdl = status.settings.thresholds.bgTargetTop >= 30 ? 1 : MMOL_TO_MGDL
   return {
     nightscoutTitle: status.settings.customTitle,
     nightscoutUrl,
     displayUnits: status.settings.units,
     targetRangeMgdl: [status.settings.thresholds.bgTargetBottom, status.settings.thresholds.bgTargetTop].map(
-      (x: number) => x * multiplier
+      (x: number) => x * targetRangeConversionToMgdl
     ) as [number, number],
   }
 }
